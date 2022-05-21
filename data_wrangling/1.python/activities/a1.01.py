961f9be64eb1fd972fada866f22308d024c97500
Dajebbar <dajebbar@gmx.fr>
2022-05-21 12:59:34 +0100

    test

--- data_wrangling/1.python/activities/a1.01.py
+++ data_wrangling/1.python/activities/a1.01.py
@@ -13,7 +13,12 @@ def find_mean(lst):
     return sum(lst)/len(lst)
 
 if __name__=='__main__':
+    dif = []
     l = 100
-    l1 = set_random_list(l)
-    l2 = by_three(l1)
-    dif = abs(len(l1) - len(l2))
+    for i in range(10):
+        l1 = set_random_list(l)
+        l2 = by_three(l1)
+        dif.append(abs(len(l1) - len(l2)))
+    
+    print(dif)
+    print(f'the mean is {find_mean(dif)}')
