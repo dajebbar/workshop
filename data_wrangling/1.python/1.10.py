1b3145011c93aeb00ed8a5f2aa84c392d5135e33
Dajebbar <dajebbar@gmx.fr>
2022-05-21 14:20:32 +0100

    from lst to dict

---
+++ data_wrangling/1.python/1.10.py
@@ -0,0 +1,9 @@
+dico = {k:k**2 for k in range(1,10)}
+print(dico)
+
+# Generate a dictionary using the dict function
+dict_2 = dict([('Tom', 100), ('Dick', 200), ('Harry', 300)])
+print(dict_2)
+
+dict_3 = dict(Tom=100, Dick=200, Harry=300)
+print(dict_3)
