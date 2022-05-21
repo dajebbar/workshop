be3a5464500138e11e75e5bce9f8222064217df8
Dajebbar <dajebbar@gmx.fr>
2022-05-21 11:09:51 +0100

    deal with lists

--- data_wrangling/1.python/1.01.py
+++ data_wrangling/1.python/1.01.py
@@ -3,3 +3,10 @@ import pandas as pd
 
 ssn = list(pd.read_csv('./datasets/ssn.csv'))
 print(ssn)
+print(ssn[0])
+print(ssn[3])
+print(ssn[-1])
+print(f'Length of ssn list: {len(ssn)}')
+print(ssn[1:3])
+# reverse elements
+print(ssn[-1::-1])
