a5e80c5e33e34d6da2a83012dee58dcc13b8e96e
Dajebbar <dajebbar@gmx.fr>
2022-05-21 11:47:54 +0100

    check list items

---
+++ data_wrangling/1.python/1.03.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+cm = list(pd.read_csv('./datasets/car_models.csv'))
+print(cm)
+
+# Check whether the strings D150 and Mustang are in the list
+print('D150' in cm)
+print('Mustang' in cm)
