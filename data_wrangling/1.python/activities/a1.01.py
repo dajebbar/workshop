768b2806e7ca212a8dbd61ea8af99245a7a333c6
Dajebbar <dajebbar@gmx.fr>
2022-05-21 12:56:53 +0100

    mean func

--- data_wrangling/1.python/activities/a1.01.py
+++ data_wrangling/1.python/activities/a1.01.py
@@ -1,8 +1,7 @@
 import random
-from random import seed
-import math
+# from random import seed
 
-seed(42)
+# seed(42)
 
 def set_random_list(l):
     return [random.randint(0, 50) for _ in range(l)]
@@ -11,4 +10,10 @@ def by_three(l):
     return [x for x in l if x % 3 ==0]
 
 def find_mean(lst):
-    pass
+    return sum(lst)/len(lst)
+
+if __name__=='__main__':
+    l = 100
+    l1 = set_random_list(l)
+    l2 = by_three(l1)
+    dif = abs(len(l1) - len(l2))
