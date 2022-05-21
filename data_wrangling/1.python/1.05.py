8df9f525935fd07ae2bc34d5e3a153409f430ded
Dajebbar <dajebbar@gmx.fr>
2022-05-21 12:39:23 +0100

    seed

--- data_wrangling/1.python/1.05.py
+++ data_wrangling/1.python/1.05.py
@@ -1,5 +1,11 @@
 import random
+from random import seed
 
+seed(42)
 
 lst_1 = [random.randint(0, 30) for _ in range(101)]
 print(lst_1)
+
+# find the square of each element
+lst_2 = [x**2 for x in lst_1]
+print(lst_2)
