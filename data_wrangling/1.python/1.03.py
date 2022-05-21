302e2bf552802d98c4584a8d8501ba10737f77b8
Dajebbar <dajebbar@gmx.fr>
2022-05-21 12:01:39 +0100

    pythonic way to iterating over loop

--- data_wrangling/1.python/1.03.py
+++ data_wrangling/1.python/1.03.py
@@ -6,3 +6,7 @@ print(cm)
 # Check whether the strings D150 and Mustang are in the list
 print('D150' in cm)
 print('Mustang' in cm)
+
+# pythonic way to iterating over loop
+for item in cm:
+    print(item)
