65b507c076a68662eb441da01dcfcf3008fc18cb
Dajebbar <dajebbar@gmx.fr>
2022-05-22 12:47:14 +0100

    text

--- data_wrangling/2.advanced_DS/2.03.py
+++ data_wrangling/2.advanced_DS/2.03.py
@@ -10,3 +10,13 @@ url_stack = []
 
 # s = ['a', 'm', 'e', 'n', 'd', 'i', 'n', 'e']
 # print(stack_pop(s))
+
+wikipedia_datascience = """Data science is an interdisciplinary
+field that uses scientific methods, processes, algorithms and systems
+to extract knowledge [https://en.wikipedia.org/wiki/Knowledge] and
+insights from data [https://en.wikipedia.org/wiki/Data] in various
+forms, both structured and unstructured,similar to data mining
+[https://en.wikipedia.org/wiki/Data_mining]"""
+
+print(f'the length: {len(wikipedia_datascience)}')
+
