fe543db8da65f652231fb3fe00dcabef5f6f4675
Dajebbar <dajebbar@gmx.fr>
2022-05-22 14:33:34 +0100

    deque

--- data_wrangling/2.advanced_DS/2.07.py
+++ data_wrangling/2.advanced_DS/2.07.py
@@ -1,13 +1,39 @@
 import time
+from collections import deque
 
 
 # Implementing a queue
-
+print("---Queue as list---")
 queue = []
 start = time.time()
 for item in range(0, 100000):
     queue.append(item)
 end = time.time()
 print("Queue created")
-print(queue)
+# print(queue)
 print(end-start)
+
+start = time.time()
+for _ in range(0, 100000):
+    queue.pop(0)
+end = time.time()
+print(end-start)
+print('queue 1 empted')
+
+print("---Queue as deque---")
+d = deque()
+start = time.time()
+for item in range(0, 100000):
+    d.append(item)
+end = time.time()
+print("Queue created")
+# print(d)
+print(end-start)
+
+start = time.time()
+for _ in range(0, 100000):
+    d.popleft()
+end = time.time()
+print(end-start)
+print('queue 2 empted')
+
