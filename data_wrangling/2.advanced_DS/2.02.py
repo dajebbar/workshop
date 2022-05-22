c1b17255c5a6bd0abb0e7c582d0b77109d12a892
Dajebbar <dajebbar@gmx.fr>
2022-05-22 12:31:49 +0100

    create a stack

---
+++ data_wrangling/2.advanced_DS/2.02.py
@@ -0,0 +1,13 @@
+stack = []
+
+stack.append('my_test@test.edu')
+stack.append('rahul.subhramanian@test.edu')
+stack.append('sania.test@test.edu')
+stack.append('alec_baldwin@test.edu')
+stack.append('albert90@test.edu')
+stack.append('stewartj@test.edu')
+
+print(stack)
+
+tos = stack.pop()
+print(tos)
