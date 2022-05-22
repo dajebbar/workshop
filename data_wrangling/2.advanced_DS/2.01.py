30f7931aed96af70e604210e4480bf9f94f6057d
Dajebbar <dajebbar@gmx.fr>
2022-05-22 12:10:08 +0100

    small memory with repeat

--- data_wrangling/2.advanced_DS/2.01.py
+++ data_wrangling/2.advanced_DS/2.01.py
@@ -1,7 +1,14 @@
 from sys import getsizeof
+from itertools import repeat
 
 big_lst = [1 for _ in range(1, int(1e7))]
 # print(big_lst)
 
 size_big_lst = getsizeof(big_lst)
 print(size_big_lst)
+
+# get less memory
+
+small_lst = repeat(1, times=int(1e7))
+size_small_lst = getsizeof(small_lst)
+print(size_small_lst)
