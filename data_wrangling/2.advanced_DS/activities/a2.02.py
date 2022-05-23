8386baf98b4d20fd052f7cb557d2927da29822f9
Dajebbar <dajebbar@gmx.fr>
2022-05-23 10:18:38 +0100

    csv parser

--- data_wrangling/2.advanced_DS/activities/a2.02.py
+++ data_wrangling/2.advanced_DS/activities/a2.02.py
@@ -2,14 +2,18 @@ import itertools
 
 
 def header_line(header, line):
-    itertools.zip_longest(header, line, fillvalue=None)
+    zipped_line = itertools.zip_longest(header, line, fillvalue=None)
+    dico = {k[0]:k[-1] for k in zipped_line}
+    return dico
 
 
 with open('../../datafiles/sales_record.csv', 'r') as f:
-    header = ''
-    for line in f.readlines()[0][:-1]:
-        header += line
-        # for word in line:
-        #     header.append(word)
-    
-    print(header.split(','))
+    first_line = f.readline()
+    header = first_line.replace("\n", "").split(",")
+    for i, line in enumerate(f):
+        # Here we loop over the first 10 lines in order to not to make the output too big
+        line = line.replace("\n", "").split(",")
+        d = header_line(header, line)
+        print(d)
+        if i > 10:
+            break
