c16196a8befa3d6dd620438af7e4ebb6c7b2de8a
Dajebbar <dajebbar@gmx.fr>
2022-05-23 09:10:00 +0100

    read file

--- data_wrangling/2.advanced_DS/2.09.py
+++ data_wrangling/2.advanced_DS/2.09.py
@@ -1,9 +1,10 @@
-# fd = open('../datafiles/data_temporary_files.txt', 'r')
-# for line in fd.readlines():
-#     for word in line:
-#         print(word, end='')
-# fd.close()
+fd = open('../datafiles/data_temporary_files.txt', 'r')
+for line in fd.readlines():
+    for word in line:
+        print(word, end='')
+fd.close()
 
 with open('../datafiles/AA.txt', 'rb') as f:
     for line in f.readline():
         print(line, end='')
+
