abde968a767390af6a712797ebe589dc53101e85
Dajebbar <dajebbar@gmx.fr>
2022-05-23 08:28:42 +0100

    get all in function

--- data_wrangling/2.advanced_DS/activities/a2.01.py
+++ data_wrangling/2.advanced_DS/activities/a2.01.py
@@ -6,6 +6,16 @@ perms = itertools.permutations([0, 1, 2])
 #         assert 'Element is not a tuple'
 #     print(i)
 
-for i in perms:
-    print(list(itertools.dropwhile(lambda x: x <= 0, i)))
+# for i in perms:
+#     print(list(itertools.dropwhile(lambda x: x <= 0, i)))
 
+
+def get_content_numbers(stack_lst):
+    num_gen = 0
+    for i in range(len(stack_lst)):
+        num_gen += stack_lst.pop() * 10**i
+    return float(num_gen)
+
+for i in perms:
+    stack_lst = list(itertools.dropwhile(lambda x: x <= 0, i))
+    print(get_content_numbers(stack_lst))
