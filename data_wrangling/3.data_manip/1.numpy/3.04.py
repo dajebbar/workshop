7776239792d57cd20805e93925d5fd8abc89eb74
Dajebbar <dajebbar@gmx.fr>
2022-05-23 12:48:39 +0100

    sine value

--- data_wrangling/3.data_manip/1.numpy/3.04.py
+++ data_wrangling/3.data_manip/1.numpy/3.04.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('../../datafiles/numbers.csv', names=['nums'])
-# print(df.head())
-
+print(df.head())
+print('\n*** * 9\n')
 lst5 = df.to_numpy().tolist()
-# print(lst5[:5])
-
+print(lst5[:5])
+print('\n*** * 9\n')
 arr5 = np.array(lst5)
 print(arr5[:5])
+print('\n*** * 9\n')
+
+# find the sine of arr5
+
+print(np.sin(arr5))
