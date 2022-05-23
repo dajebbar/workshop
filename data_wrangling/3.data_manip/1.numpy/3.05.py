c62dd7ca34d9934ab64962a34a1d144275f26636
Dajebbar <dajebbar@gmx.fr>
2022-05-23 12:54:23 +0100

    np arange spaced with float

---
+++ data_wrangling/3.data_manip/1.numpy/3.05.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+a = np.arange(5, 16)
+print(a)
+
+print("Numbers spaced apart by 2: ", np.arange(0,11,2))
+print("Numbers spaced apart by a floating point number: ", np.arange(0,11,2.5))
+print("Every 5th number from 30 in reverse order\n", np.arange(30,-1,-5))
