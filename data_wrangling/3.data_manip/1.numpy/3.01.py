bc51a0894a4557aaa9d1f4f72b66ca85fe3bc6ac
Dajebbar <dajebbar@gmx.fr>
2022-05-23 11:23:48 +0100

    read from numpy

--- data_wrangling/3.data_manip/1.numpy/3.01.py
+++ data_wrangling/3.data_manip/1.numpy/3.01.py
@@ -2,7 +2,10 @@ import numpy as np
 
 lst1 = [1,2,3]
 arr1 = np.array(lst1)
-print(arr1)
+# print(arr1)
 
 a = np.array([1.2, 3.4, 5.6])
-print(a, type(a))
+# print(a, type(a))
+
+data = np.genfromtxt('../../datafiles/stock.csv', delimiter=',', names=True, dtype=None, encoding='ascii')
+print(data)
