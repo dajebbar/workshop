b4c87f87e611c795e9b48f5d36e52e04a152afb2
Dajebbar <dajebbar@gmx.fr>
2022-05-23 09:56:27 +0100

    read header

--- data_wrangling/2.advanced_DS/activities/a2.02.py
+++ data_wrangling/2.advanced_DS/activities/a2.02.py
@@ -3,3 +3,13 @@ import itertools
 
 def header_line(header, line):
     itertools.zip_longest(header, line, fillvalue=None)
+
+
+with open('../../datafiles/sales_record.csv', 'r') as f:
+    header = ''
+    for line in f.readlines()[0][:-1]:
+        header += line
+        # for word in line:
+        #     header.append(word)
+    
+    print(header.split(','))
