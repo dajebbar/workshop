8b8d4b0678006c72e1a5889bb741c359cec5e503
Dajebbar <dajebbar@gmx.fr>
2022-05-23 17:27:31 +0100

    add new columns

---
+++ data_wrangling/3.data_manip/2.pandas/3.16.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv('../../datafiles/stock.csv')
+# print(df.head())
+df['New'] = df.Price * 2
+# print(df.head())
+df['New (Sum of X and Z)'] = df.New + df.Price
+print(df.head())
