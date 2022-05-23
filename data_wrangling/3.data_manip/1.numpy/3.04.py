4b15c266dcbc05d856b3b83efd49983ae1bc63fe
Dajebbar <dajebbar@gmx.fr>
2022-05-23 12:44:30 +0100

    numpy array

--- data_wrangling/3.data_manip/1.numpy/3.04.py
+++ data_wrangling/3.data_manip/1.numpy/3.04.py
@@ -1,8 +1,11 @@
 import pandas as pd
-
+import numpy as np
 
 df = pd.read_csv('../../datafiles/numbers.csv', names=['nums'])
 # print(df.head())
 
 lst5 = df.to_numpy().tolist()
-print(lst5[:5])
+# print(lst5[:5])
+
+arr5 = np.array(lst5)
+print(arr5[:5])
