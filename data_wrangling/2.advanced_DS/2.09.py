5a9db423131c8cccd45864eebd47b4a1c22eed4e
Dajebbar <dajebbar@gmx.fr>
2022-05-23 09:06:21 +0100

    open file as binary

---
+++ data_wrangling/2.advanced_DS/2.09.py
@@ -0,0 +1,9 @@
+# fd = open('../datafiles/data_temporary_files.txt', 'r')
+# for line in fd.readlines():
+#     for word in line:
+#         print(word, end='')
+# fd.close()
+
+with open('../datafiles/AA.txt', 'rb') as f:
+    for line in f.readline():
+        print(line, end='')
