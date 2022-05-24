0b5838bb29817f0befc73aa89719578a04ca2f02
Dajebbar <dajebbar@gmx.fr>
2022-05-24 09:39:48 +0100

    scatter age weight

--- data_wrangling/3.data_manip/3.matplotlib/3.17.py
+++ data_wrangling/3.data_manip/3.matplotlib/3.17.py
@@ -9,3 +9,11 @@ age = [21,12,32,45,37,18,28,52,5,40,48,15]
 weight = [55,35,77,68,70,60,72,69,18,65,82,48]
 height = [160,135,170,165,173,168,175,159,105,171,155,158]
 
+# age versus weight
+
+plt.scatter(x=age, y=weight)
+plt.title('Age vs weight')
+plt.xlabel('Age')
+plt.ylabel('Weight')
+
+plt.show()
