8a95ca8e7229e93837435b09549dba1c8df1892d
Dajebbar <dajebbar@gmx.fr>
2022-05-24 12:25:58 +0100

    plot normal dist

--- data_wrangling/3.data_manip/3.matplotlib/3.20.py
+++ data_wrangling/3.data_manip/3.matplotlib/3.20.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,3 +9,12 @@ plt.style.use('fivethirtyeight')
 # print(x)
 
 student_heights = np.random.normal(loc=155, scale=10, size=100)
+
+# plotting
+plt.figure(figsize=(12,8))
+plt.title('Young Students height', fontsize=16)
+plt.hist(x=student_heights, color='k')
+plt.xlabel('heights(cm)', fontsize=16)
+plt.ylabel('freq', fontsize=16)
+
+plt.show()
