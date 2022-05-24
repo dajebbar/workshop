743d2bc41f08fda8617485f187da74428e82fae1
Dajebbar <dajebbar@gmx.fr>
2022-05-24 12:20:53 +0100

    students height

--- data_wrangling/3.data_manip/3.matplotlib/3.20.py
+++ data_wrangling/3.data_manip/3.matplotlib/3.20.py
@@ -4,5 +4,7 @@ import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 
 
-x = np.random.normal()
-print(x)
+# x = np.random.normal()
+# print(x)
+
+student_heights = np.random.normal(loc=155, scale=10, size=100)
