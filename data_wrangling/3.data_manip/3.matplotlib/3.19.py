e69d06c1dd38ced19803313485054c8359634e36
Dajebbar <dajebbar@gmx.fr>
2022-05-24 12:00:54 +0100

    bar plot

--- data_wrangling/3.data_manip/3.matplotlib/3.19.py
+++ data_wrangling/3.data_manip/3.matplotlib/3.19.py
@@ -5,4 +5,12 @@ plt.style.use('fivethirtyeight')
 
 
 x = np.random.binomial(10, 0.6, 8)
-print(x)
+# print(x)
+
+plt.figure(figsize=(7,4))
+plt.title('Number of successes in coin toss', fontsize=16)
+plt.bar(x=np.arange(1, 9), height=x)
+plt.xlabel('Experiment number', fontsize=16)
+plt.ylabel('Number of succes', fontsize=16)
+
+plt.show()
