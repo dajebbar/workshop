4afc781af34d6ee9d4f9655a316d5c99a42f202d
Dajebbar <dajebbar@gmx.fr>
2022-05-24 09:36:01 +0100

    data

---
+++ data_wrangling/3.data_manip/3.matplotlib/3.17.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+
+people = ['Ann','Brandon','Chen','David','Emily',
+        'Farook','Gagan','Hamish','Imran','Joseph',
+        'Katherine','Lily']
+age = [21,12,32,45,37,18,28,52,5,40,48,15]
+weight = [55,35,77,68,70,60,72,69,18,65,82,48]
+height = [160,135,170,165,173,168,175,159,105,171,155,158]
+
