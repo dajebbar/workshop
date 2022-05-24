df8c64567a2697b9f4c07c46833ebf168fb9caea
Dajebbar <dajebbar@gmx.fr>
2022-05-24 11:39:17 +0100

    uniform distribution

---
+++ data_wrangling/3.data_manip/3.matplotlib/3.18.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+
+
+# genrate random
+x = np.random.randint(1,10, 1)
+# print(x)
+x = np.round(50 + 50 *np.random.random(size=15), 2)
+print(x)
