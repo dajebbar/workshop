3e8f335cbe27bbae5160c5173b7aaba244dea8c9
dajebbar <dajebbar@gmx.fr>
2022-06-16 13:22:12 +0100

    extract text

--- 1-requestsHandsOn/main.py
+++ 1-requestsHandsOn/main.py
@@ -4,6 +4,7 @@ response = requests.get('https://quotes.toscrape.com/')
 # print(response.status_code)
 # print(response.encoding)
 html = response.text
+tag = '<span class="text" itemprop="text">'
 for line in html.split('\n'):
-    if '<span class="text" itemprop="text">' in line:
-        print(line)
+    if tag in line:
+        print(line.replace(tag, '').replace('“', '').replace('”', '').replace('</span>', ''))
