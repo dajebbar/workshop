5494a380f3aacea00ea49fa58d7c7ce652584ff3
dajebbar <dajebbar@gmx.fr>
2022-06-16 13:17:49 +0100

    class text

--- 1-requestsHandsOn/main.py
+++ 1-requestsHandsOn/main.py
@@ -1,6 +1,9 @@
 import requests
 
 response = requests.get('https://quotes.toscrape.com/')
-print(response.status_code)
-print(response.encoding)
-print(response.text)
+# print(response.status_code)
+# print(response.encoding)
+html = response.text
+for line in html.split('\n'):
+    if '<span class="text" itemprop="text">' in line:
+        print(line)
