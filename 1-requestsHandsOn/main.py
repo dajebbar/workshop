15b1e22044b009f4c0a6363c3bd38b9c53fe1c69
dajebbar <dajebbar@gmx.fr>
2022-06-16 14:41:15 +0100

    extract multi-pages

--- 1-requestsHandsOn/main.py
+++ 1-requestsHandsOn/main.py
@@ -1,14 +1,16 @@
 import requests
 
-response = requests.get('https://quotes.toscrape.com/')
-# print(response.status_code)
-# print(response.encoding)
-html = response.text
-tag = '<span class="text" itemprop="text">'
-with open('quotes.txt', 'a') as f:
-    for line in html.split('\n'):
-        if tag in line:
-            line = line.replace(tag, '').replace('“', '').replace('”', '').replace('</span>', '').strip()
-            f.write(line)
-            f.write('\n')
-    print('Done!')
+for i in range(1, 11):
+    print(f'page {i}...')
+    response = requests.get(f'https://quotes.toscrape.com/page/{i}/')
+    # print(response.status_code)
+    # print(response.encoding)
+    html = response.text
+    tag = '<span class="text" itemprop="text">'
+    with open('quotes.txt', 'a') as f:
+        for line in html.split('\n'):
+            if tag in line:
+                line = line.replace(tag, '').replace('“', '').replace('”', '').replace('</span>', '').strip()
+                f.write(line)
+                f.write('\n')
+        print('Done!')
