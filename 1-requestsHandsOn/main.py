1baff850482ead345417fe55874295004126c3e2
dajebbar <dajebbar@gmx.fr>
2022-06-16 13:33:34 +0100

    extract text qotes in new file

--- 1-requestsHandsOn/main.py
+++ 1-requestsHandsOn/main.py
@@ -5,6 +5,10 @@ response = requests.get('https://quotes.toscrape.com/')
 # print(response.encoding)
 html = response.text
 tag = '<span class="text" itemprop="text">'
-for line in html.split('\n'):
-    if tag in line:
-        print(line.replace(tag, '').replace('“', '').replace('”', '').replace('</span>', '').strip())
+with open('quotes.txt', 'a') as f:
+    for line in html.split('\n'):
+        if tag in line:
+            line = line.replace(tag, '').replace('“', '').replace('”', '').replace('</span>', '').strip()
+            f.write(line)
+            f.write('\n')
+    print('Done!')
