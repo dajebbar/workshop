846c856036361711df25c4d46f41092d8182fd28
dajebbar <dajebbar@gmx.fr>
2022-06-16 13:49:02 +0100

    extract authors name

--- 1-requestsHandsOn/authors.py
+++ 1-requestsHandsOn/authors.py
@@ -5,9 +5,12 @@ r = requests.get('https://quotes.toscrape.com/')
 html = r.text
 
 tag = '<small class="author" itemprop="author">'
+
 with open('authors.txt', 'w') as f:
     for line in html.split('\n'):
         if tag in line:
+            line = line.replace(tag, '').replace('<span>', '').replace('</small>', '')
             line = line.strip()
-            f.write(line)
+            f.write(line[2:])
             f.write('\n')
+    print('Done!')
