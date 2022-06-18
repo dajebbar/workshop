bc1317c2da5a9a0194936980340d9ee9dc408b1a
dajebbar <dajebbar@gmx.fr>
2022-06-18 12:29:47 +0100

    extract movies rating

--- 2-bs4/imdb.py
+++ 2-bs4/imdb.py
@@ -11,7 +11,8 @@ soup = BeautifulSoup(response.content, 'html.parser')
 principal_tag = soup.find('tbody', {'class':'lister-list'}).find_all('tr')
 movie_name = [tag.find('td', {'class':'titleColumn'}).find('a').get_text().strip() for tag in principal_tag]
 movie_year = [tag.find('td', {'class':'titleColumn'}).find('span', {'class':'secondaryInfo'}).get_text().replace('(','').replace(')','').strip() for tag in principal_tag]
-
-print(movie_name)
-print('-*-' * 50)
-print(movie_year)
+movie_rating = [tag.find('td', {'class':'ratingColumn imdbRating'}).find('strong').get_text().strip() for tag in principal_tag]
+# print(movie_name)
+# print('-*-' * 50)
+# print(movie_year)
+print(movie_rating)
