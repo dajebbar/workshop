5fedb014c8cce45c83bce84bd84855569c50fd43
dajebbar <dajebbar@gmx.fr>
2022-06-18 12:58:09 +0100

    add movie link

--- 2-bs4/imdb.py
+++ 2-bs4/imdb.py
@@ -12,12 +12,14 @@ principal_tag = soup.find('tbody', {'class':'lister-list'}).find_all('tr')
 movie_name = [tag.find('td', {'class':'titleColumn'}).find('a').get_text().strip() for tag in principal_tag]
 movie_year = [tag.find('td', {'class':'titleColumn'}).find('span', {'class':'secondaryInfo'}).get_text().replace('(','').replace(')','').strip() for tag in principal_tag]
 movie_rating = [tag.find('td', {'class':'ratingColumn imdbRating'}).find('strong').get_text().strip() for tag in principal_tag]
+link = 'https://www.imdb.com/'
+movie_link = [link+tag.find('td', {'class':'titleColumn'}).find('a').get('href') for tag in principal_tag]
 # print(movie_name)
 # print('-*-' * 50)
 # print(movie_year)
 # print(movie_rating)
 
-df = pd.DataFrame({'movies': movie_name, 'years': movie_year, 'ratings':movie_rating})
+df = pd.DataFrame({'movies': movie_name, 'years': movie_year, 'ratings':movie_rating, 'links':movie_link})
 # print(df.sample(10))
 
 df.to_csv('imbd_top250.csv', index=False)
