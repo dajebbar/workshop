9e088639169dd82513be9bda0c56c09ccddea94d
dajebbar <dajebbar@gmx.fr>
2022-06-18 10:03:38 +0100

    fix some tags

--- 2-bs4/p1.py
+++ 2-bs4/p1.py
@@ -18,8 +18,8 @@ soup = BeautifulSoup(response.content, 'html.parser')
 principal_tag = soup.find_all('article', {'class':'product_pod'})
 book_img_link = [url+img.find('div', {'class':'image_container'}).find('a').get('href') for img in principal_tag]
 book_title = [ttle.find('h3').find('a').get('title') for ttle in principal_tag]
-book_price = [price.find('div', {'class': 'product_price'}).find('p',{'class':'price_color'}).get_text().replace('£','').strip() for price in principal_tag]
-book_in_store = [stock.find('div', {'class': 'product_price'}).find('p',{'class':'instock availability'}).get_text().strip() for stock in principal_tag]
+book_price = [price.find('p',{'class':'price_color'}).get_text().replace('£','').strip() for price in principal_tag]
+book_in_store = [stock.find('p',{'class':'instock availability'}).get_text().strip() for stock in principal_tag]
 
 # print(book_img_link)
 # print(book_title)
