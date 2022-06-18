dd152bd0900dc5a13f1c182fb0c873ac6628f7e8
dajebbar <dajebbar@gmx.fr>
2022-06-18 09:09:01 +0100

    books in stock

--- 2-bs4/p1.py
+++ 2-bs4/p1.py
@@ -17,10 +17,11 @@ soup = BeautifulSoup(response.content, 'html.parser')
 principal_tag = soup.find_all('article', {'class':'product_pod'})
 book_img_link = [url+img.find('div', {'class':'image_container'}).find('a').get('href') for img in principal_tag]
 book_title = [ttle.find('h3').find('a').get('title') for ttle in principal_tag]
-book_price = [price.find('div', {'class': 'product_price'}).find('p', {'class':'price_color'}).get_text().replace('£','').strip() for price in principal_tag]
-
+book_price = [price.find('div', {'class': 'product_price'}).find('p',{'class':'price_color'}).get_text().replace('£','').strip() for price in principal_tag]
+book_in_store = [stock.find('div', {'class': 'product_price'}).find('p',{'class':'instock availability'}).get_text().strip() for stock in principal_tag]
 # for img in principal_tag:
 #     print(img.find('div', {'class':'image_container'}).find('a').get('href'))
 # print(book_img_link)
 # print(book_title)
-print(book_price)
+# print(book_price)
+print(book_in_store)
