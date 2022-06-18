39fba5551022e8d0553a4d1eaf1d77d99391c965
dajebbar <dajebbar@gmx.fr>
2022-06-18 09:05:53 +0100

    books price

--- 2-bs4/p1.py
+++ 2-bs4/p1.py
@@ -17,9 +17,10 @@ soup = BeautifulSoup(response.content, 'html.parser')
 principal_tag = soup.find_all('article', {'class':'product_pod'})
 book_img_link = [url+img.find('div', {'class':'image_container'}).find('a').get('href') for img in principal_tag]
 book_title = [ttle.find('h3').find('a').get('title') for ttle in principal_tag]
-book_price = []
+book_price = [price.find('div', {'class': 'product_price'}).find('p', {'class':'price_color'}).get_text().replace('£','').strip() for price in principal_tag]
 
 # for img in principal_tag:
 #     print(img.find('div', {'class':'image_container'}).find('a').get('href'))
 # print(book_img_link)
-print(book_title)
+# print(book_title)
+print(book_price)
