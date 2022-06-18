476b2422a5de9932d6e5a812bfe006d751ddfa09
dajebbar <dajebbar@gmx.fr>
2022-06-18 14:25:22 +0100

    director page link

--- 2-bs4/imdb2.py
+++ 2-bs4/imdb2.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 url = 'https://www.imdb.com/title/tt0111161/'
 resp2 = requests.get(url)
 soup2 = BeautifulSoup(resp2.content, 'html.parser')
-
+director_url = 'https://www.imdb.com'
 # print(resp2.status_code)
 
 init_tag = soup2.find('section',{'class': 'ipc-page-section ipc-page-section--baseAlt ipc-page-section--tp-none ipc-page-section--bp-xs sc-910a7330-1 iPKxCm'})
@@ -17,3 +17,4 @@ movie_director_link = init_tag.find('a',{'class':'ipc-metadata-list-item__list-c
 print(movie_time.get_text())
 print(movie_genre.get_text())
 print(movie_director)
+print(director_url+movie_director_link)
